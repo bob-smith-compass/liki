0cd3ad13781572d155b016cda0256485ce2f8b37
bob-smith-compass <david.shams@gmail.com>
2019-07-07 10:24:07 -0500

    outline-width: 50px;     outline-style: outset;

---
+++ src/components/links/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './links.css'
+
+export default function Links() {
+    return (
+        <details>
+            <summary>
+                React Hookes
+            </summary>
+            <a href="https://reactjs.org/docs/hooks-overview.html#rules-of-hooks">rules-of-hooks</a>
+        </details>
+    )
+}
