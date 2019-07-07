8c905448e146b6a8e6fba83575ac4f185fb1b124
bob-smith-compass <david.shams@gmail.com>
2019-07-07 17:53:30 -0500

    Context API in action     <ConfigContext.Provider values={configValues}>

--- src/App.js
+++ src/App.js
@@ -9,25 +9,32 @@ import Todo from './components/todo/Todo';
 /**
  * Context API
  */
-const ConfigContext = React.createContext();
+const ConfigContext = React.createContext(); // Hookes will be used to consume this context
+const configValues = {
+  name: 'David Shams',
+  primaryImage: 'https://images.unsplash.com/photo-1562337589-136d8a7dd9dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  secondaryImage: 'https://images.unsplash.com/photo-1562351768-f68650f3ec54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+}
 
 
 function App() {
   return (
-    <div className="container">
-      <Todo />
-      <div className="well">
-        <Links />
-        <ImageToggle
-          primaryImage={'https://images.unsplash.com/photo-1562337589-136d8a7dd9dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
-          secondaryImage={'https://images.unsplash.com/photo-1562351768-f68650f3ec54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
-        />
-        <div>
-          <span className="LrzXr kno-fv">274 m/s<sup>2</sup>; 28 × Earth</span>
+    <ConfigContext.Provider values={configValues}>
+      <div className="container">
+        <Todo />
+        <div className="well">
+          <Links />
+          <ImageToggle
+            primaryImage={configValues.primaryImage}
+            secondaryImage={configValues.secondaryImage}
+          />
+          <div>
+            <span className="LrzXr kno-fv">274 m/s<sup>2</sup>; 28 × Earth</span>
+          </div>
+          <Events />
         </div>
-        <Events />
       </div>
-    </div>
+    </ConfigContext.Provider>
   );
 }
 
