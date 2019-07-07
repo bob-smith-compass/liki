f391e679c0bb9ccfb5bc6bd2db964831378d51d2
bob-smith-compass <david.shams@gmail.com>
2019-07-07 10:34:21 -0500

    bootstrap included with React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ImageToggle from './components/image-tggle/ImageToggle';
 import Events from './components/events/Events';
 import Links from './components/links/Links';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <div className="jumbotron">
+
       <h3>LIKI</h3>
       <p>All LIKI classmates with pictures</p>
       <p>Display current picture initially - upon hover display old 1984 pictures</p>
@@ -21,17 +24,21 @@ function App() {
       <p>Search with mongoDB</p>
       <p>Relay GraphQL</p>
       <p>Debounce</p>
-      <Links />
+      </div>
 
-      <ImageToggle 
-        primaryImage={'https://images.unsplash.com/photo-1562337589-136d8a7dd9dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
-        secondaryImage={'https://images.unsplash.com/photo-1562351768-f68650f3ec54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
+      <div>
+        <Links />
+
+        <ImageToggle
+          primaryImage={'https://images.unsplash.com/photo-1562337589-136d8a7dd9dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
+          secondaryImage={'https://images.unsplash.com/photo-1562351768-f68650f3ec54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'}
         />
         <div>
-        <span className="LrzXr kno-fv">274 m/s<sup>2</sup>; 28 × Earth</span>
+          <span className="LrzXr kno-fv">274 m/s<sup>2</sup>; 28 × Earth</span>
         </div>
         <Events />
-    
+
+      </div>
     </div>
   );
 }
